ed76ab0c191af3366cd84b7cb68ee1c3ec23a56d
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-12 20:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ class Person{
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
-
 class Employee extends Person {
   constructor(name, age, jobTitle) {
     super(name, age);
     this.jobTitle = jobTitle;
   }
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 
